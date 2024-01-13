668ea5c0baa629beb8004369991996eed6ab9ad9
saeideh abbaszadeh <saeideh676@gmail.com>
2024-01-13 14:17:04 +0330

    put form inputs inside another component named: OrderFormInputs

---
+++ app/(further-information)/_components/OrderFormInputs.tsx
@@ -0,0 +1,49 @@
+import { Divider, ErrorMessage, Input } from '@/components'
+import Address from './Address'
+import { useFormContext } from 'react-hook-form'
+import { infer as zInfer } from 'zod'
+import { formSchema } from '../page'
+
+export default function OrderFormInputs() {
+  const {
+    register,
+    setValue,
+    clearErrors,
+    formState: { errors },
+  } = useFormContext<zInfer<typeof formSchema>>()
+
+  return (
+    <div>
+      <p>لطفا اطلاعات شخصی مالک خودرو را وارد کنید:</p>
+      <Divider className="mb-4" />
+
+      <div className="flex flex-col gap-[22px] mb-8">
+        <div className="flex flex-col">
+          <Input
+            placeholder="کد ملی"
+            {...register('nationalCode')}
+            error={!!errors.nationalCode}
+          />
+          <ErrorMessage text={errors.nationalCode?.message} />
+        </div>
+
+        <div className="flex flex-col">
+          <Input
+            placeholder="شماره تلفن همراه"
+            {...register('phoneNumber')}
+            error={!!errors.phoneNumber}
+          />
+          <ErrorMessage text={errors.phoneNumber?.message} />
+        </div>
+      </div>
+      <Address
+        onSelectAddress={address => {
+          setValue('address', address.id)
+          clearErrors('address')
+        }}
+      />
+      <input type="hidden" {...register('address')} />
+      <ErrorMessage text={errors.address?.message} />
+    </div>
+  )
+}
--- app/(further-information)/page.tsx
+++ app/(further-information)/page.tsx
@@ -1,16 +1,16 @@
 'use client'
-import { Button, Divider, ErrorMessage, Input } from '@/components'
-import Address from './_components/Address'
+import { Button } from '@/components'
 import { infer as zInfer, object, string } from 'zod'
-import { useForm } from 'react-hook-form'
+import { useForm, FormProvider } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { mobilePhone, nationalCode } from '@/schemas/global-schema'
 import { useCreateOrderCompletion } from '@/api/orderCompletion'
 import { useState } from 'react'
 import { cn } from '@/utils'
 import Message from './_components/Message'
+import OrderFormInputs from './_components/OrderFormInputs'
 
-const formSchema = object({
+export const formSchema = object({
   nationalCode: nationalCode(),
   phoneNumber: mobilePhone(),
   address: string().min(1, { message: 'آدرس نمی‌تواند خالی باشد' }),
@@ -20,13 +20,7 @@ export default function FurtherInformation() {
   const [showMsg, setShowMsg] = useState<false | 'success' | 'error'>(false)
   const [errMsg, setErrMsg] = useState<string>('')
 
-  const {
-    register,
-    setValue,
-    clearErrors,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<zInfer<typeof formSchema>>({
+  const methods = useForm<zInfer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       nationalCode: '',
@@ -73,49 +67,20 @@ export default function FurtherInformation() {
       )}
       {/* order completion form */}
       {
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className={cn(
-            'px-5 py-8 flex flex-col justify-between gap-8 h-full',
-            showMsg && 'hidden',
-          )}
-        >
-          <div>
-            <p>لطفا اطلاعات شخصی مالک خودرو را وارد کنید:</p>
-            <Divider className="mb-4" />
-
-            <div className="flex flex-col gap-[22px] mb-8">
-              <div className="flex flex-col">
-                <Input
-                  placeholder="کد ملی"
-                  {...register('nationalCode')}
-                  error={!!errors.nationalCode}
-                />
-                <ErrorMessage text={errors.nationalCode?.message} />
-              </div>
-
-              <div className="flex flex-col">
-                <Input
-                  placeholder="شماره تلفن همراه"
-                  {...register('phoneNumber')}
-                  error={!!errors.phoneNumber}
-                />
-                <ErrorMessage text={errors.phoneNumber?.message} />
-              </div>
-            </div>
-            <Address
-              onSelectAddress={address => {
-                setValue('address', address.id)
-                clearErrors('address')
-              }}
-            />
-            <input type="hidden" {...register('address')} />
-            <ErrorMessage text={errors.address?.message} />
-          </div>
-          <Button color="secondary" className="self-end" type="submit">
-            تایید و ادامه
-          </Button>
-        </form>
+        <FormProvider {...methods}>
+          <form
+            onSubmit={methods.handleSubmit(onSubmit)}
+            className={cn(
+              'px-5 py-8 flex flex-col justify-between gap-8 h-full',
+              showMsg && 'hidden',
+            )}
+          >
+            <OrderFormInputs />
+            <Button color="secondary" className="self-end" type="submit">
+              تایید و ادامه
+            </Button>
+          </form>
+        </FormProvider>
       }
     </>
   )
